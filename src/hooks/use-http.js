b817680ebd4dfa4ea83a2306a292f057600fe0e9
lytvyn-dev <frontend.lytvyn@gmail.com>
2023-04-04 22:39:38 +0300

    get-1

---
+++ src/hooks/use-http.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const useHttp = (requestOptions, onGetData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const request = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(requestOptions.url, {
+        method: requestOptions.method ? requestOptions.method : "GET",
+        body: requestOptions.body ? JSON.stringify(requestOptions.body) : null,
+        headers: requestOptions.headers ? requestOptions.headers : {},
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      onGetData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    error,
+    request,
+  };
+};
+
+export default useHttp;
