56700310d2ed6ad2b59c14dc82a7a8d06e430cc4
lytvyn-dev <frontend.lytvyn@gmail.com>
2023-04-05 06:54:03 +0300

    done

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,42 +1,27 @@
-import { useState } from "react";
-
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, request: sendRequst } = useHttp();
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-app-9a38a-default-rtdb.firebaseio.com/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+  const enterTaskHandler = (taskText) => {
+    const options = {
+      url: "https://react-app-9a38a-default-rtdb.firebaseio.com/tasks.json",
+      method: "POST",
+      body: { text: taskText },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
+    const getData = (data) => {
       const generatedId = data.name; // firebase-specific => "name" contains generated id
       const createdTask = { id: generatedId, text: taskText };
-
       props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+    };
+
+    sendRequst(options, getData);
   };
 
   return (
